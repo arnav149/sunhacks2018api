9fed64e88b732e28b88da2e1b1d31c46e57259ff
Arnav Kasturia <arnavkasturia@hotmail.com>
2018-11-11 06:51:49 -0700

    adding for the first time

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+const cors = require('cors');
+const axios = require('axios');
+const direction = require('google-maps-direction');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'speedmaster'
+  }
+});
+
+let datab = {
+  lat:'',
+  long:'',
+  endlat:0,
+  endlong:0
+}
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/driver',(req,res)=>{
+  console.log(req.body);
+  datab.lat = parseFloat(req.body.lat);
+  datab.long = parseFloat(req.body.long);
+  res.send('success');
+})
+
+app.get('/parent',(req,res)=>{
+  if(datab.lat!=0 && datab.long!=0)
+    res.send({lat: datab.lat,long: datab.long})
+
+
+})
+
+app.post('/latitude',(req,res)=>{
+  datab.endlat = req.body.endlat;
+  datab.endlong = req.body.endlong;
+  console.log(datab.endlat,datab.endlong);
+  res.send('success');
+})
+
+app.get('/speed',(req,res)=>{
+  let obj={
+    distance:0,
+    time:0
+  };
+  axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${datab.lat},${datab.long}&destinations=${datab.endlat},${datab.endlong}&key=AIzaSyApsiMJBsI6piScQHEWkYOEbUU211S2M30`,{mode:'no-cors'})
+  //.then(dat=> res.send({dist:dat.data.rows[0].elements[0].distance.value,tm:dat.data.rows[0].elements[0].time.value}))
+  .then(dat=> res.send({dist:dat.data.rows[0].elements[0].distance.value,time:dat.data.rows[0].elements[0].duration.value}))
+  //.then(dat=> console.log(dat.data.rows[0].elements[0]))
+  .catch(err=>console.log(err))
+  // direction({
+  //   origin:`${datab.lat},${datab.long}`,
+  //   destination:`${req.body.endlat},${req.body.endlong}`
+  // })
+  // .then(function(result){
+  //   console.log(result);
+//})
+
+})
+
+
+
+app.listen(3000,()=>console.log('app is running on port 3000'));
